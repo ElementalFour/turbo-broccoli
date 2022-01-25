a0d68282fa93b6be1dcc85598adfb9d6b1d9f3ef
ElementalFour <91990918+ElementalFour@users.noreply.github.com>
2022-01-25 13:51:31 -0500

    Add files via upload

---
+++ Amogus.java
@@ -0,0 +1,20 @@
+package amogus;
+
+public class Amogus 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int x = 1;x < 1000;x++) {
+			amogus();
+			System.out.println("Amogus #" + x);
+		}
+	}
+	
+	public static void amogus() 
+	{
+		System.out.println("\n __________\n|          |\n|    ________\n|"
+				+ "   |        |\n|   |________|\n|          |\n|  _____   |"
+				+ "\n|  |   |   |\n|__|   |___|");
+	}
+}
